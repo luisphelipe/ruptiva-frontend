de1d4443bb6bead2d9d3a4d6ddc50ee9d33bc92b
Luis Phelipe <scphelipe@gmail.com>
2020-09-20 19:39:32 -0300

    Add signup page implementation

--- src/screens/auth/signup/index.tsx
+++ src/screens/auth/signup/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  FlexColumnExpand,
+  BookImage,
+  FlexColumn,
+  Input,
+  Button,
+  Link,
+} from "../style";
 
-const Signup = () => {
-  return <div>Signup</div>;
+const Login = () => {
+  return (
+    <FlexColumnExpand>
+      <BookImage />
+      <FlexColumn>
+        <label htmlFor="email">E-mail</label>
+        <Input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password</label>
+        <Input id="password" name="password" type="password" required />
+      </FlexColumn>
+      <FlexColumn alignItems="center">
+        <Button onClick={() => console.log("TODO: submit signup")}>
+          SIGNUP
+        </Button>
+        <Link to="/login">...OR LOGIN</Link>
+      </FlexColumn>
+    </FlexColumnExpand>
+  );
 };
 
-export default Signup;
+export default Login;
