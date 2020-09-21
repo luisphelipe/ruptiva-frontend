93b287f2b773a6092fc5d18e238ba8ffc5b341ac
Luis Phelipe <scphelipe@gmail.com>
2020-09-20 22:20:34 -0300

    Add localStorage support for auth token

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import AuthContext from "../contexts/auth.context";
 
 import { login, signup } from "../api/auth.api";
+const TOKEN_STORAGE_PATH = "@ruptiva-luis-phelipe/token";
 
 const AuthService = ({ children }: { children: any }) => {
   const [token, setToken] = useState("");
@@ -10,6 +11,7 @@ const AuthService = ({ children }: { children: any }) => {
     try {
       const res = await login(email, password);
       await setToken(res.data.token);
+      localStorage.setItem(TOKEN_STORAGE_PATH, res.data.token);
     } catch (err) {
       if (err.response.status === 406)
         return {
@@ -28,6 +30,7 @@ const AuthService = ({ children }: { children: any }) => {
     try {
       const res = await signup(email, password);
       setToken(res.data.token);
+      localStorage.setItem(TOKEN_STORAGE_PATH, res.data.token);
     } catch (err) {
       if (err.response.status === 406)
         return {
@@ -44,12 +47,12 @@ const AuthService = ({ children }: { children: any }) => {
 
   const logout = async () => {
     setToken("");
-    // TODO: Remove token from localStorage
+    localStorage.setItem(TOKEN_STORAGE_PATH, "");
   };
 
   useEffect(() => {
-    // TODO: Load token from localStorage
-    console.log("TODO: LOAD TOKEN FROM LOCAL STORAGE");
+    const storage_token = localStorage.getItem(TOKEN_STORAGE_PATH);
+    if (storage_token) setToken(storage_token);
   }, []);
 
   return (
